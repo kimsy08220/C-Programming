e890b24a6e0d943dd37d8fb57c0bb5317ea8bbbd
kimsy08220 <kimsy08220@naver.com>
2021-09-26 23:13:30 +0900

    누구나 쉽게 즐기는 C언어 콘서트

---
+++ 누구나 쉽게 즐기는 C언어 콘서트/Chapter2.기초 사항/3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int price, num;
+
+	printf("상품 가격을 입력하시오 : ");
+	scanf_s("%d", &price);
+	printf("상품의 개수를 입력하시오 : ");
+	scanf_s("%d", &num);
+
+	printf("총 가격은 %d입니다.\n", price * num);
+
+	return 0;
+}
