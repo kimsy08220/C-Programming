a4468363bf2435370fe67738afbfe9c19d77c1f1
kimsy08220 <kimsy08220@naver.com>
2021-10-31 00:51:15 +0900

    누구나 쉽게 즐기는 C언어 콘서트

---
+++ 누구나 쉽게 즐기는 C언어 콘서트/Chapter1.프로그래밍 소개/OCU_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>	// 표준 입출력 함수가 선언된 헤더파일을 포함
+
+int main(void)
+{
+	printf("HELLO! My first C program.\n\r");				// \n : 다음 줄로 줄바꿈(Line feed), \r : 그 줄의 처음으로 이동(Carriage return)
+	printf("김수연 goes for a good C programmer.\n\r");		// 큰 따옴표 안에 있는 문자열을 출력
+	return(0);	// 정상종료
+}
+
